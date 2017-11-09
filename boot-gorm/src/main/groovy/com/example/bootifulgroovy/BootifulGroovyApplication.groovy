1b17274cf0ca6f228371874eae1d9885da76588c
Jeff Scott Brown <brownj@objectcomputing.com>
2017-11-09 08:24:07 -0600

    start gormification

--- boot-gorm/src/main/groovy/com/example/bootifulgroovy/BootifulGroovyApplication.groovy
+++ boot-gorm/src/main/groovy/com/example/bootifulgroovy/BootifulGroovyApplication.groovy
@@ -1,5 +1,6 @@
 package com.example.bootifulgroovy
 
+import grails.gorm.annotation.Entity
 import groovy.transform.EqualsAndHashCode
 import groovy.transform.ToString
 import org.springframework.beans.factory.annotation.Autowired
@@ -7,18 +8,14 @@ import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Controller
+import grails.gorm.services.Service
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.ModelAndView
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
 
 @SpringBootApplication
 class BootifulGroovyApplication {
@@ -68,19 +65,15 @@ class ReservationMvcController {
     }
 }
 
-interface ReservationRepository extends JpaRepository<Reservation, Long> {
-
-    Collection<Reservation> findByReservationName(@Param("rn") String rn)
+@Service(Reservation)
+interface ReservationRepository  {
+    Collection<Reservation> findByReservationName(String reservationName)
+    Collection<Reservation> findAll()
 }
 
 @Entity
 @ToString
 @EqualsAndHashCode
 class Reservation {
-
-    @Id
-    @GeneratedValue
-    Long id
-
     String reservationName
 }
